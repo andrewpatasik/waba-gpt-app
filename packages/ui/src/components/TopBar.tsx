d498634d9fe4832bf6e699ec8fb2f7ffa1c2a53f
Andrew <andrew.yehezkiel17@gmail.com>
2024-11-10 20:05:59 +0800

    feat: add fetching adapter

--- packages/ui/src/components/TopBar.tsx
+++ packages/ui/src/components/TopBar.tsx
@@ -1,11 +1,26 @@
-import { Zap } from "lucide-react";
+import { Zap, Dot, User, Phone } from "lucide-react";
 
-const TopBar = () => {
+type topBarProps = {
+  data: any;
+};
+
+const TopBar = ({ data }: topBarProps) => {
+
   return (
     <nav className="fixed w-full h-11 border-b flex items-center p-2">
       <p className="text-sm flex space-x-1 items-center text-slate-400">
         <Zap className="w-4 fill-yellow-400 text-yellow-400" />
         <span className="font-semibold">100 credits</span>
+      </p>
+      <Dot className="text-slate-400" />
+      <p className="flex space-x-1 items-center text-sm text-slate-400">
+        <User className="w-4" />
+        <span>{data.verified_name}</span>
+      </p>
+      <Dot className="text-slate-400" />
+      <p className="flex space-x-1 items-center text-sm text-slate-400">
+        <Phone className="w-4" />
+        <span>{data.display_phone_number}</span>
       </p>
     </nav>
   );
--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import axios from "axios";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const fetchData = async (pathname?:string) => {
+  try {
+    const headers = {
+      Authorization: `Bearer ${process.env.WABA_SYSTEM_TOKEN}`,
+    };
+
+    const response = await axios(`${process.env.BASE_URL}/484528214737576`, {
+      headers,
+    });
+
+    if (response.status !== 200)
+      throw new Error("something not right: ", { cause: response.statusText });
+
+    return {
+      status: response.status,
+      message: response.statusText,
+      data: response.data,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
