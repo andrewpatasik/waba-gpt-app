60972528f3cd83940521edb2736f8ba0cad02041
Andrew <andrew.yehezkiel17@gmail.com>
2024-11-18 17:12:29 +0800

    feat:webhook: add config to test

--- packages/backend/webhook/routes/index.js
+++ packages/backend/webhook/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.setHeader('Content-Type', 'application/json')
+  res.send({ text: 'ok'})
 });
 
 module.exports = router;
---
+++ packages/backend/webhook/routes/webhook.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function (req, res, next) {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === process.env.VERIFICATION_TOKEN) {
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+module.exports = router;
